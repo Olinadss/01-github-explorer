420851e405e41904cf3eab038419012ad202dd88
Danilo dos Santos <danilo.santossouza@outlook.com>
2022-10-22 12:07:23 -0300

    revendo o useEffect e fazendo uma chamada para api do github

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,31 +1,31 @@
-const repositoryName = 'unform'
+import { RepositoryItem } from "./RepositoryItem";
+import { useState, useEffect } from "react";
+
+import '../styles/repositories.scss'
+
+const repository = {
+  name: 'unform',
+  description: 'Form in React',
+  link: 'https://github.com/Olinadss/01-github-explorer'
+}
 
 export function RepositoryList() {
+  const [repositories, setRepositories] = useState([]);
+  
+  useEffect(() => {
+    fetch('https://api.github.com/orgs/rocketseat/repos')
+      .then(response => response.json)
+      .then(data => setRepositories(data));
+  }, []);
+
   return (
     <section className="repository-list">
       <h1>Lista de repositórios</h1>
       <ul>
-        <li>
-          <strong>{repositoryName}</strong>
-          <p>Forms in React</p>
-          <a href="">
-            Acessar repositórios
-          </a>
-        </li>
-        <li>
-          <strong>{repositoryName}</strong>
-          <p>Forms in React</p>
-          <a href="">
-            Acessar repositórios
-          </a>
-        </li>
-        <li>
-          <strong>{repositoryName}</strong>
-          <p>Forms in React</p>
-          <a href="">
-            Acessar repositórios
-          </a>
-        </li>
+        <RepositoryItem repository={repository} />
+        <RepositoryItem repository={repository} />
+        <RepositoryItem repository={repository} />
+        <RepositoryItem repository={repository} />
       </ul>
     </section>
   )
